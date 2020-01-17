aaec9dcf88395d4f283c96667deba5eb70a792b1
Andrew Tribone <tribone@easypost.com>
2020-01-17 15:37:37 -0800

    favicon

--- tribone/app.py
+++ tribone/app.py
@@ -1,6 +1,11 @@
+import os
+
 from dotenv import load_dotenv
-from flask import Flask
-from flask import render_template
+from flask import (
+    Flask,
+    render_template,
+    send_from_directory
+)
 
 
 load_dotenv()
@@ -12,3 +17,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    print(os.path.join(app.root_path, 'static'))
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
