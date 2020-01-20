9b4819cb55e3b704fe9fe9af6b2f6d2674356489
Andrew Tribone <tribone@easypost.com>
2020-01-19 21:53:21 -0800

    pki

--- tribone/app.py
+++ tribone/app.py
@@ -26,9 +26,17 @@ def resume():
 
 @app.route('/favicon.ico')
 def favicon():
-    print(os.path.join(app.root_path, 'static'))
     return send_from_directory(
         os.path.join(app.root_path, 'static'),
         'favicon.ico',
         mimetype='image/vnd.microsoft.icon'
+    )
+
+
+@app.route('/.well-known/pki-validation/18FA44ACD11780D440CDDF9880E184F1.txt')
+def pki():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        '18FA44ACD11780D440CDDF9880E184F1.txt',
+        mimetype='text/plain'
     )
